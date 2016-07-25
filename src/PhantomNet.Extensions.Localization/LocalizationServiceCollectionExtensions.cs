4d9a28b6992ac9aebfbef661a643f8c7d038f75b
Minh Nguyen <mnguyen284@gmail.com>
2016-07-25 17:26:17 +0700

    Initial commit

---
+++ src/PhantomNet.Extensions.Localization/LocalizationServiceCollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Localization;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class LocalizationServiceCollectionExtensions
+    {
+        public static IServiceCollection AddLocalization(this IServiceCollection services, string defaultCulture, params string[] supportedCultures)
+        {
+            if (string.IsNullOrWhiteSpace(defaultCulture))
+            {
+                throw new ArgumentNullException(nameof(defaultCulture));
+            }
+
+            services.AddLocalization(options => options.ResourcesPath = "Resources")
+                    .AddSingleton<IStringLocalizerFactory, AssemblyAwareResourceManagerStringLocalizerFactory>();
+
+            services.Configure<RequestLocalizationOptions>(options => {
+                options.DefaultRequestCulture = new RequestCulture(culture: defaultCulture, uiCulture: defaultCulture);
+                var cultures = supportedCultures.Select(x => new CultureInfo(x)).ToArray();
+                options.SupportedCultures = cultures;
+                options.SupportedUICultures = cultures;
+            });
+
+            return services;
+        }
+    }
+}
