c9b82fa45514b43a1de23e2c2230f4c4dff6b6e1
Minh Nguyen <mnguyen284@gmail.com>
2017-09-26 23:07:45 +0700

    Build

--- Build.ps1
+++ Build.ps1
@@ -32,11 +32,11 @@ $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 # exec { & dotnet test .\test\PhantomNet.Core.Tests -c Release }
 
 if ($env:APPVEYOR_REPO_TAG -eq $true) {
-	exec { & dotnet pack .\src\aspnet\PhantomNet.Extensions.Localization -c Release -o ..\..\artifacts }
+	exec { & dotnet pack .\src\aspnet\PhantomNet.Extensions.Localization -c Release -o ..\..\..\artifacts }
 } else {
 	$revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 	$revision = "{0:D4}" -f [convert]::ToInt32($revision, 10);
 	$suffix = "beta-" + $revision
 
-	exec { & dotnet pack .\src\aspnet\PhantomNet.Extensions.Localization -c Release -o ..\..\artifacts --version-suffix=$suffix }
+	exec { & dotnet pack .\src\aspnet\PhantomNet.Extensions.Localization -c Release -o ..\..\..\artifacts --version-suffix=$suffix }
 }
